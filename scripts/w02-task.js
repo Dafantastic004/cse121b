bcf7507c534c9fd80fe1209596de27849853f6c4
Dafantastic004 <144632188+Dafantastic004@users.noreply.github.com>
2024-01-18 08:28:46 +0100

    Add files via upload

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,17 +3,27 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-
+let fullName='Momoh Destiny';
+let currentYear='2024';
+let ProfilePic='images/pic.png';
 
 
 
 /* Step 3 - Element Variables */
-
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img')
 
 
 
 
 /* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute('src', ProfilePic);
+imageElement.setAttribute('alt',`profile image of ${fullName}`);
+
 
 
 
@@ -21,9 +31,13 @@
 
 
 /* Step 5 - Array */
-
-
-
-
-
+const favouriteFoods = ['Rice', 'Beans', 'Semo', 'yam', 'Dodo', 'Spag']
+foodElement.innerHTML=favouriteFoods
+const favFood = ['Banana']
+favouriteFoods.push(favFood);
+foodElement.innerHTML += `<br>${favouriteFoods}`;
+favouriteFoods.shift(favFood);
+foodElement.innerHTML += `<br>${favouriteFoods}`;
+favFood.pop();
+foodElement.innerHTML += `<br>${favouriteFoods}`;
 
